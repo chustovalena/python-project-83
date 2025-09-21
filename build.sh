46e779adb05ffe39e4d3365792af59c127648395
Елена Чустова <elenacustova@MacBook-Air-Elena.local>
2025-09-21 22:28:12 +0300

    update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 make install
 
-make install && psql -a -d $DATABASE_URL -f database.sq
+make install && psql -a -d $DATABASE_URL -f database.sql
