7949f6b7813eb7b99d686c3450a31892bd67d611
Елена Чустова <elenacustova@MacBook-Air-Elena.local>
2025-09-21 22:28:44 +0300

    add url_cheks table

--- database.sql
+++ database.sql
@@ -3,3 +3,14 @@ CREATE TABLE IF NOT EXISTS urls (
     name varchar(255) UNIQUE NOT NULL,
     created_at date DEFAULT CURRENT_DATE
 );
+
+
+CREATE TABLE IF NOT EXISTS url_checks (
+    id SERIAL PRIMARY KEY,
+    url_id int NOT NULL REFERENCES urls(id),
+    status_code int,
+    h1 varchar(255),
+    title varchar(255),
+    description text,
+    created_at date DEFAULT CURRENT_DATE
+);
