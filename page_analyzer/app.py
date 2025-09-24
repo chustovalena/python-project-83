60d478caeb7c0ba76df482edc05448cddb5480eb
Елена Чустова <elenacustova@MacBook-Air-Elena.local>
2025-09-24 23:08:52 +0300

    from app to create_app()

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -17,99 +17,95 @@ from repository import UrlRepository
 from page_analyzer.log_conf import RequestFormatter, RequestFilter
 
 
-app = Flask(__name__)
-formatter = RequestFormatter(
-    "%(asctime)s [%(levelname)s] %(remote_addr)s %(method)s %(path)s: %(message)s"
-)
-handler = logging.StreamHandler()
-handler.setFormatter(formatter)
-handler.addFilter(RequestFilter())
+def create_app():
+    app = Flask(__name__)
 
-app.logger.setLevel(logging.INFO)
-app.logger.addHandler(handler)
-
-load_dotenv()
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
-app.debug = True
-
-repo = UrlRepository(app.config['DATABASE_URL'])
-
-
-@app.route('/')
-def index():
-    app.logger.info('Запрос к /')
-    messages = get_flashed_messages(with_categories=True)
-    return render_template(
-        'index.html',
-        messages=messages
+    formatter = RequestFormatter(
+        "%(asctime)s [%(levelname)s] %(remote_addr)s %(method)s %(path)s: %(message)s"
     )
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    handler.addFilter(RequestFilter())
+    app.logger.setLevel(logging.INFO)
+    app.logger.addHandler(handler)
 
+    load_dotenv()
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
+    app.debug = True
 
-@app.route('/urls')
-def get_urls():
-    app.logger.info('Запрос к /urls')
-    messages = get_flashed_messages(with_categories=True)
-    urls = repo.get_content()
-    return render_template('urls/index.html', urls=urls, messages=messages)
+    repo = UrlRepository(app.config['DATABASE_URL'])
 
-
-@app.post('/urls')
-def new_url():
-    app.logger.info('POST запрос к /urls')
-    url = request.form.to_dict()
-    errors = validate(url)
-    if errors:
-        for error in errors:
-            flash(error, "danger")
-        return redirect(url_for('index'))
-    saved = repo.save(url)
-    if saved:
-        flash('Url был успешно добавлен', 'success')
-    else:
-        flash('Url уже существует', 'error')
-    return redirect(url_for('get_urls'))
-
-
-@app.route('/urls/<url_id>')
-def get_url(url_id):
-    app.logger.info('Запрос к /urls/<url_id>')
-    messages = get_flashed_messages(with_categories=True)
-    url = repo.find(url_id)
-    if not url:
-        abort(404)
-    checks = repo.get_checks_with_id(url_id)
-
-    return render_template(
-        'urls/show.html',
-        url=url,
-        checks=checks,
-        messages=messages
-    )
-
-
-@app.post('/urls/<int:url_id>/checks')
-def check(url_id):
-    app.logger.info(f'POST запрос к /urls/{url_id}/checks')
-    url = repo.find(url_id)
-    if not url:
-        flash('No such id', 'error')
-        return redirect(url_for('get_urls'))
-    try:
-        response = requests.get(url['name'], timeout=5)
-        response.raise_for_status()
-        result = perform_check(url['name'])
-        repo.new_check(
-            url_id,
-            result['status_code'],
-            result['h1'],
-            result['title'],
-            result['description']
+    @app.route('/')
+    def index():
+        app.logger.info('Запрос к /')
+        messages = get_flashed_messages(with_categories=True)
+        return render_template(
+            'index.html',
+            messages=messages
         )
-        flash('Проверка успешно выполнена', 'success')
-    except Exception:
-        app.logger.exception('Failed to create url_check')
-        flash('Cannot crate check', 'error')
-        return redirect(url_for('get_url', url_id=url_id))
 
-    return redirect(url_for('get_url', url_id=url_id))
+    @app.route('/urls')
+    def get_urls():
+        app.logger.info('Запрос к /urls')
+        messages = get_flashed_messages(with_categories=True)
+        urls = repo.get_content()
+        return render_template('urls/index.html', urls=urls, messages=messages)
+
+    @app.post('/urls')
+    def new_url():
+        app.logger.info('POST запрос к /urls')
+        url = request.form.to_dict()
+        errors = validate(url)
+        if errors:
+            for error in errors:
+                flash(error, "danger")
+            return redirect(url_for('index'))
+        saved = repo.save(url)
+        if saved:
+            flash('Url был успешно добавлен', 'success')
+        else:
+            flash('Url уже существует', 'error')
+        return redirect(url_for('get_urls'))
+
+    @app.route('/urls/<url_id>')
+    def get_url(url_id):
+        app.logger.info('Запрос к /urls/<url_id>')
+        messages = get_flashed_messages(with_categories=True)
+        url = repo.find(url_id)
+        if not url:
+            abort(404)
+        checks = repo.get_checks_with_id(url_id)
+
+        return render_template(
+            'urls/show.html',
+            url=url,
+            checks=checks,
+            messages=messages
+        )
+
+    @app.post('/urls/<int:url_id>/checks')
+    def check(url_id):
+        app.logger.info(f'POST запрос к /urls/{url_id}/checks')
+        url = repo.find(url_id)
+        if not url:
+            flash('No such id', 'error')
+            return redirect(url_for('get_urls'))
+        try:
+            response = requests.get(url['name'], timeout=5)
+            response.raise_for_status()
+            result = perform_check(url['name'])
+            repo.new_check(
+                url_id,
+                result['status_code'],
+                result['h1'],
+                result['title'],
+                result['description']
+            )
+            flash('Проверка успешно выполнена', 'success')
+        except Exception:
+            app.logger.exception('Failed to create url_check')
+            flash('Cannot crate check', 'error')
+            return redirect(url_for('get_url', url_id=url_id))
+
+        return redirect(url_for('get_url', url_id=url_id))
