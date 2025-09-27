be1a21eb6a30b6514e75e0fd77c6588a59e19957
Елена Чустова <elenacustova@MacBook-Air-Elena.local>
2025-09-27 12:49:47 +0300

    create tests for app

---
+++ tests/test_app.py
@@ -0,0 +1,50 @@
+import pytest
+
+
+def test_get_main(client):
+    response = client.get('/')
+
+    assert response.status_code == 200
+    assert b"SEO" in response.data
+
+
+def test_get_urls(client):
+    response = client.get('/urls')
+
+    assert response.status_code == 200
+    assert b'http://' not in response.data
+
+
+def test_post_urls(client):
+    response = client.post('/urls', data={"url": "http://example.com"})
+    resp = client.get('/urls')
+
+    assert response.status_code == 302
+    assert b"http://example.com" in resp.data
+
+
+def test_get_url(client):
+    client.post('/urls', data={"url": "http://example.com"})
+    response = client.get('/urls/1')
+
+    assert b"http://example.com" in response.data
+    assert response.status_code == 200
+
+
+def test_no_duple(client):
+    client.post('/urls', data={"url": "http://example.com"})
+    client.post('/urls', data={"url": "http://example.com"})
+
+    response = client.get('/urls')
+    assert response.data.count(b'http://example.com') == 1
+
+
+
+def test_check(client):
+    client.post('/urls', data={"url": "https://web.mit.edu"})
+    response = client.post('/urls/1/checks')
+    assert response.status_code == 302
+    response = client.get('/urls/1')
+    assert b'MIT' in response.data
+
+
