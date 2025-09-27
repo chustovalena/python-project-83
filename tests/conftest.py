464450ae795861f14eabf307c87baa38fa3575db
Елена Чустова <elenacustova@MacBook-Air-Elena.local>
2025-09-27 12:49:18 +0300

    fix db bugs

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,45 @@
 import os
 import pytest
+import psycopg
 from page_analyzer.app import create_app
+from repository import UrlRepository
+
+
+@pytest.fixture(scope='session')
+def db_url():
+    return os.environ.get(
+        'TEST_DATABASE_URL',
+        "postgresql://test_user:test_pass@localhost:5432/test_db"
+    )
+
+
+@pytest.fixture(scope='session', autouse=True)
+def setup_test_db(db_url):
+    with psycopg.connect(db_url) as conn:
+        with conn.cursor() as cur:
+            with open('database.sql', 'r') as f:
+                schema_sql = f.read()
+                cur.execute(schema_sql)
+            conn.commit()
+
+
+@pytest.fixture(autouse=True)
+def clean_tables(db_url):
+    with psycopg.connect(db_url) as conn:
+        with conn.cursor() as cur:
+            cur.execute("TRUNCATE url_checks, urls RESTART IDENTITY CASCADE;")
+        conn.commit()
+    yield
 
 
 @pytest.fixture
-def app():
-    os.environ['DATABASE_URL'] = "postgresql://test_user:test_pass@localhost:5432/test_db"
-    os.environ['SECRET_KEY'] = 'test-secret'
-    app = create_app()
+def repo(db_url):
+    return UrlRepository(db_url)
+
+
+@pytest.fixture
+def app(db_url):
+    app = create_app(db_url=db_url)
     app.config['TESTING'] = True
     return app
 
