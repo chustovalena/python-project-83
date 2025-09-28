5f4519e9f15a0026950212475a0a07058130f13b
Елена Чустова <elenacustova@MacBook-Air-Elena.local>
2025-09-28 21:36:43 +0300

    update validaye

--- page_analyzer/validate.py
+++ page_analyzer/validate.py
@@ -1,11 +1,19 @@
 from urllib.parse import urlparse
 
 
-def validate(url):
-    errors = {}
-    if len(url['url']) > 255:
-        errors['name'] = 'Url length must be less than 256 characters'
-    parsed = urlparse(url['url'])
-    if not parsed.scheme or not parsed.netloc:
-        errors['name'] = 'Incorrect URL'
+def validate(form):
+    errors = []
+    url = form.get('url', '').strip()
+
+    if not url:
+        errors.append('URL is required')
+
+    elif len(url) > 255:
+        errors.append('URL length must be less than 256 characters')
+
+    else:
+        parsed = urlparse(url)
+        if parsed.scheme not in ('http', 'https') or not parsed.netloc:
+            errors.append('Incorrect URL')
+
     return errors
